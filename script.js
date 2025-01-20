44ce6a19b1da2a51e1121db92bd0b8f11260e10b
tonyeeeeezy <Fiorean1145@gmail.com>
2025-01-19 18:31:33 -0800

    script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function showDetails(personId) {
+    const details = document.getElementById(personId);
+    if (details.style.display === 'none' || details.style.display === '') {
+        details.style.display = 'block';
+    } else {
+        details.style.display = 'none';
+    }
+}
